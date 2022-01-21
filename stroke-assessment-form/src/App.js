d007737548034114feff9c95210a527106fa90ca
Nabil Abdul Hadi <nabil.hadi69@gmail.com>
2022-01-21 20:41:28 +0800

    back to function one

--- stroke-assessment-form/src/App.js
+++ stroke-assessment-form/src/App.js
@@ -1,7 +1,7 @@
 import StrokeForm from './page/StrokeForm';
 import './App.css';
 import PopUp from './component/element/PopUp';
-
+import React from 'react';
 function App() {
   return (
     <div>
--- stroke-assessment-form/src/component/element/PopUp.js
+++ stroke-assessment-form/src/component/element/PopUp.js
@@ -3,15 +3,17 @@ import React from 'react'
 export default function PopUp({title, description, modal}) {
     return (  
         <div class="modal" id='myModal'>
-            <div className="modal-dialog modal-dialog-center">
+            <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title border-bottom">{title}</h5>
                         <button className="btn-close" data-bs-dismiss={modal}></button>
                     </div>
+                    <br />
                     <div className="modal-body">
                         <p>{description}</p>
                     </div>
+                    <br />
                     <div className="modal-footer">
                         <button className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </div>
